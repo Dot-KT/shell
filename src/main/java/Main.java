c19ed49da1410dc02c44301c106ab4723b0cc3e7
Ridwan.kt <7devanle@gmail.com>
2025-11-24 14:06:10 +0100

    added the echo command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,18 @@ public class Main {
 
     static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        String command = "exit";
         while(true){
             System.out.print("$ ");
-            command = new Scanner(System.in).nextLine();
-            if(command.equals("exit")) break;
-            System.out.println(command + ": command not found");
+            String command = new Scanner(System.in).nextLine();
+            if(command.startsWith("echo ")){
+                System.out.println(command.substring(5));
+                continue;
+            }
+            switch (command){
+                case "exit" -> {return;}
+                default -> {System.out.println(command + ": command not found");}
+            }
+
         }
     }
 }
