71627a5abb50db1c518c389648b392cb23a21335
Ridwan.kt <7devanle@gmail.com>
2025-11-24 08:20:48 +0100

    appended errors to all commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
+        System.out.print("$ ");
+        String command = new Scanner(System.in).next();
+        System.out.println(command + ": command not found");
     }
 }
