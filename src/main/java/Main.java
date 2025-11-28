f9bfa0c9f78b41605d1fa17fae119cd94b739833
Ridwan.kt <7devanle@gmail.com>
2025-11-28 06:41:22 +0100

    implemneted the necessary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,51 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
+    private static List<String> shellCommands = new ArrayList<>(Arrays.asList("echo", "exit", "type"));
+    private static Scanner scanner = new Scanner(System.in);
 
-    static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
+    public static void main(String[] args) throws Exception {
         while(true){
             System.out.print("$ ");
-            String command = new Scanner(System.in).nextLine();
-            if(command.startsWith("echo ")){
-                System.out.println(command.substring(5));
-                continue;
-            }
-            switch (command){
+            String command = scanner.nextLine();
+            String firstCommand = splitCommand(command, "first");
+            switch (firstCommand){
+                case "echo" -> System.out.println(splitCommand(command, ""));
+                case "type" -> System.out.println(checkType(splitCommand(command, "")));
                 case "exit" -> {return;}
-                default -> {System.out.println(command + ": command not found");}
+                default -> System.out.println(command + ": command not found");
             }
-
         }
+    }
+    public static String checkType(String type){
+        if (shellCommands.contains(type)){
+            return type + " is a shell builtin";}
+        else {
+            return checkOriginalType(type);
+        }
+    }
+    public static String checkOriginalType(String command){
+        String path = System.getenv("PATH");
+        if(path == null) return "invalid_command: not found";
+        String[] pathCommands = path.split(File.pathSeparator);
+        for(String pathCommand : pathCommands){
+            File file = new File(pathCommand, command);
+            if(file.exists() && Files.isExecutable(file.toPath())) return command + " is "+file.getAbsolutePath();
+        }
+        return command+": not found";
+    }
+    public static String splitCommand(String command, String part){
+        String[] subCommands = command.split(" ");
+        if(part.equals("first")) {
+            return subCommands[0];
+        }
+        return command.substring(5);
     }
 }
