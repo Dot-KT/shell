332db58f15d4fbbdfa0d43468bdc671aaf038870
Ridwan.kt <7devanle@gmail.com>
2025-12-05 14:48:49 +0100

    working echo command

---
+++ src/main/java/Command.java
@@ -0,0 +1,112 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Command {
+    private static List<String> shellCommands = new ArrayList<>(Arrays.asList("echo", "exit", "type"));
+    private static String path = System.getenv("PATH");
+    private static String[] pathCommands = path.split(File.pathSeparator);
+
+    private String command;
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public Command(String command) {
+        this.command = command;
+    }
+    String checkType(String type) {
+        if (shellCommands.contains(type)) {
+            return type + " is a shell builtin";
+        } else {
+            return checkOriginalType(type);
+        }
+    }
+    String checkOriginalType(String command) {
+        if (path == null) return "invalid_command: not found";
+        for (String pathCommand : pathCommands) {
+            File file = new File(pathCommand, command);
+            if (file.exists() && Files.isExecutable(file.toPath())) return command + " is " + file.getAbsolutePath();
+        }
+        return command + ": not found";
+    }
+    String splitCommand(String command, String part) {
+        String[] subCommands = command.split(" ");
+        if (part.equals("first")) {
+            return subCommands[0];
+        }
+        return command.substring(5);
+    }
+    String[] splitCommand(String command) {
+        try {
+            return command.split(" ");
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    char previous = '2';
+    int checkExecutability(String command){
+        if(shellCommands.contains(command)) return 1;
+        for (String pathCommand : pathCommands) {
+            File file = new File(pathCommand, command);
+            if (file.exists() && Files.isExecutable(file.toPath())) return 2;
+        }
+        return 3;
+    }
+    void runCommand(String command){
+        int checker = checkExecutability(splitCommand(command, "first"));
+        if(checker==2){
+            String[] commands = splitCommand(command);
+            String command1 = commands[0];
+            try{
+                Process process = Runtime.getRuntime().exec(commands);
+                process.getInputStream().transferTo(System.out);
+            } catch (IOException e) {
+                System.out.println(command1 + ": not found");
+            }
+        }else{
+            System.out.println(command +": command not found");
+        }
+    }
+    List<String> readQuotes(String input){
+        boolean singleQuoteIsOpen = false;
+        boolean doubleQuoteIsOpen = false;
+        boolean isOpen = false;
+        List<String> args = new ArrayList<>();
+        String content = "";
+        for(char ch : input.toCharArray()){
+            if(ch == '\\'){}
+            else if(previous == '\\'){
+                content+=ch;
+            }
+            else if(ch == '"' && !singleQuoteIsOpen && previous!= '\\'){
+                doubleQuoteIsOpen = !doubleQuoteIsOpen;
+                isOpen = doubleQuoteIsOpen;
+            }
+            else if(ch == '\'' && !doubleQuoteIsOpen && previous!= '\\'){
+                singleQuoteIsOpen = !singleQuoteIsOpen;
+                isOpen = singleQuoteIsOpen;
+            }
+            else if(!isOpen && ch == ' '){
+                if(content.length()>0) args.add(content);
+                content = "";
+            }
+            else{
+                content+=ch;
+            }
+            previous = ch;
+        }
+        if(content.length()>0) args.add(content);
+
+        return args;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,51 +1,56 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
-    private static List<String> shellCommands = new ArrayList<>(Arrays.asList("echo", "exit", "type"));
     private static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) throws Exception {
-        while(true){
+        while (true) {
             System.out.print("$ ");
             String command = scanner.nextLine();
-            String firstCommand = splitCommand(command, "first");
-            switch (firstCommand){
-                case "echo" -> System.out.println(splitCommand(command, ""));
-                case "type" -> System.out.println(checkType(splitCommand(command, "")));
-                case "exit" -> {return;}
-                default -> System.out.println(command + ": command not found");
+            Command newCommand = new Command(command);
+            String firstPart = newCommand.splitCommand(command, "first");
+
+            switch (firstPart) {
+                case ""-> {break;}
+                case "echo" -> {
+                    String secondPart = command.replaceFirst("echo ", "");
+                    System.out.println(String.join(" ", newCommand.readQuotes(secondPart)));
+                    break;
+                }
+                case "type" -> {
+                    String secondPart = command.replaceFirst("type ", "");
+                    System.out.println(newCommand.checkType(secondPart));
+                    break;
+                }
+                case "cat" -> {
+                    try{
+                        String secondPart = command.replaceFirst("cat ", "");
+                        List<String> contents = new ArrayList<>(List.of("cat"));
+                        contents.addAll(newCommand.readQuotes(secondPart));
+                        StringBuilder builder = new StringBuilder();
+                        ProcessBuilder pBuilder = new ProcessBuilder(contents);
+                        Process process = pBuilder.start();
+                        process.getInputStream().transferTo(System.out);
+                        process.getErrorStream().transferTo(System.out);
+                        process.wait();
+                    } catch (Exception e) {
+                        System.out.print("");
+                    }
+                    break;
+                }
+                case "exit" -> {
+                    return;
+                }
+                default -> newCommand.runCommand(command);
             }
         }
-    }
-    public static String checkType(String type){
-        if (shellCommands.contains(type)){
-            return type + " is a shell builtin";}
-        else {
-            return checkOriginalType(type);
-        }
-    }
-    public static String checkOriginalType(String command){
-        String path = System.getenv("PATH");
-        if(path == null) return "invalid_command: not found";
-        String[] pathCommands = path.split(File.pathSeparator);
-        for(String pathCommand : pathCommands){
-            File file = new File(pathCommand, command);
-            if(file.exists() && Files.isExecutable(file.toPath())) return command + " is "+file.getAbsolutePath();
-        }
-        return command+": not found";
-    }
-    public static String splitCommand(String command, String part){
-        String[] subCommands = command.split(" ");
-        if(part.equals("first")) {
-            return subCommands[0];
-        }
-        return command.substring(5);
     }
 }
